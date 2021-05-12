66371cb01e4e98f05b05c951e03dabb183be409e
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-05-12 10:19:10 +0900

    add tests

---
+++ src/Counter.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public count = 1;
+    
+    function incretemenCount() public {
+        count ++;
+    }
+}
